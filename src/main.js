236b6297494e4ff8fe8f18652de02ca77bb59920
zmark <351479188@qq.com>
2024-04-13 15:30:21 +0800

    style: 缩进2空格

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import router from './router';
 import 'element-plus/dist/index.css';  // 确保引入了Element Plus的CSS
 
 createApp(App)
-    .use(router)
-    .mount('#app');
+	.use(router)
+	.mount('#app');
