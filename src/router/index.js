8b117e93a491e9ed236c1261c0e0e18fd554517f
zmark <351479188@qq.com>
2024-04-13 15:33:35 +0800

    style: 缩进使用tab-两空格

--- src/router/index.js
+++ src/router/index.js
@@ -7,32 +7,32 @@ import SignUpPage from "@/pages/SignUpPage.vue";
 
 // 定义路由
 const routes = [
-  { path: '/', component: HomePage },
-  { path: '/home', component: HomePage },
-  { path: '/about', component: AboutPage },
-  { path: '/login', component: LogInPage },
-  { path: '/signup', component: SignUpPage },
+	{ path: '/', component: HomePage },
+	{ path: '/home', component: HomePage },
+	{ path: '/about', component: AboutPage },
+	{ path: '/login', component: LogInPage },
+	{ path: '/signup', component: SignUpPage },
 ];
 
 // 创建路由实例并传递 `routes` 配置
 const router = createRouter({
-  history: createWebHistory (),
-  routes, // `routes: routes` 的缩写
+	history: createWebHistory (),
+	routes, // `routes: routes` 的缩写
 });
 
 router.beforeEach((to, from, next) => {
-  // TODO 从 store 中获取用户状态
-  // if (to.meta.requireAuth) {
-  //     if (store.state.user.username) {
-  //         next();
-  //     } else {
-  //         next({
-  //             path: 'login',
-  //             query: { redirect: to.fullPath }
-  //         });
-  //     }
-  // }
-  next();
+	// TODO 从 store 中获取用户状态
+	// if (to.meta.requireAuth) {
+	//     if (store.state.user.username) {
+	//         next();
+	//     } else {
+	//         next({
+	//             path: 'login',
+	//             query: { redirect: to.fullPath }
+	//         });
+	//     }
+	// }
+	next();
 });
 
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,17 @@
 import { reactive } from 'vue';
 
 const store = reactive({
-  state: {
-    user: {
-      username: window.localStorage.getItem('user' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('user' || '[]')).username
-    }
-  },
-  mutations: {
-    login (state, user) {
-      state.user = user;
-      window.localStorage.setItem('user', JSON.stringify(user));
-    }
-  }
+	state: {
+		user: {
+			username: window.localStorage.getItem('user' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('user' || '[]')).username
+		}
+	},
+	mutations: {
+		login (state, user) {
+			state.user = user;
+			window.localStorage.setItem('user', JSON.stringify(user));
+		}
+	}
 });
 
 export default store;
