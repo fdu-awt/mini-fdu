d8f314304ac1f2bfe2b72b46d4ff991a61e7ba4a
zmark <351479188@qq.com>
2024-04-14 02:08:02 +0800

    feat: axios中为api设置request拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { ElMessage, ElMessageBox, ElNotification } from 'element-plus';
 import router from "@/router";
+import { getToken } from "@/utils/auth";
 
 const service = axios.create({
 	baseURL: process.env.VUE_APP_BASE_URL,
@@ -9,6 +10,46 @@ const service = axios.create({
 		'Content-Type': 'application/json;charset=utf-8',
 	},
 });
+
+// request拦截器
+service.interceptors.request.use(
+	config => {
+		// 是否需要设置 token
+		const isToken = (config.headers || {}).isToken === false;
+		const token = getToken();
+		if (token && !isToken) {
+			// TODO 让每个请求携带自定义token 请根据实际情况自行修改
+			config.headers["Authorization"] = "Bearer " + token;
+		}
+		// get请求映射params参数
+		if (config.method === "get" && config.params) {
+			let url = config.url + "?";
+			for (const propName of Object.keys(config.params)) {
+				const value = config.params[propName];
+				const part = encodeURIComponent(propName) + "=";
+				if (value !== null && typeof value !== "undefined") {
+					if (typeof value === "object") {
+						for (const key of Object.keys(value)) {
+							let params = propName + "[" + key + "]";
+							const subPart = encodeURIComponent(params) + "=";
+							url += subPart + encodeURIComponent(value[key]) + "&";
+						}
+					} else {
+						url += part + encodeURIComponent(value) + "&";
+					}
+				}
+			}
+			url = url.slice(0, -1);
+			config.params = {};
+			config.url = url;
+		}
+		return config;
+	},
+	error => {
+		console.error(error);
+		Promise.reject(error).then(r => console.error(r));
+	}
+);
 
 // 响应拦截器
 service.interceptors.response.use(
