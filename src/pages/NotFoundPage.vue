d8a0f22f80d41fe92e3205bc9fbfdf9c9340156d
zmark <351479188@qq.com>
2024-04-13 16:00:26 +0800

    perf: 捕获所有未处理的路径到404page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+	name: "NotFoundPage"
+};
+</script>
+
+<template>
+	<div>404 页面，可能没有</div>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,19 @@ import AboutPage from "@/pages/AboutPage.vue";
 import LogInPage from "@/pages/LogInPage.vue";
 import SignUpPage from "@/pages/SignUpPage.vue";
 import UserProfile from "@/pages/UserProfile.vue";
+import NotFoundPage from "@/pages/NotFoundPage.vue";
 // import store from "../store";
 
 // 定义路由
 const routes = [
 	{ path: '/', component: HomePage, meta: { requireAuth: true }},
 	{ path: '/home', component: HomePage, meta: { requireAuth: true }},
-	{ path: '/about', component: AboutPage },
-	{ path: '/login', component: LogInPage },
-	{ path: '/signup', component: SignUpPage },
+	{ path: '/about', component: AboutPage, meta: { requireAuth: false }},
+	{ path: '/login', component: LogInPage, meta: { requireAuth: false }},
+	{ path: '/signup', component: SignUpPage, meta: { requireAuth: false }},
 	{ path: '/profile', component: UserProfile, meta: { requireAuth: true }},
+	// 捕获所有未处理的路径
+	{ path: '/:pathMatch(.*)*', component: NotFoundPage, meta: { requireAuth: false }},
 ];
 
 // 创建路由实例并传递 `routes` 配置
