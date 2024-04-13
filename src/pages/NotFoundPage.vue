05319aa696d6472caf9334de569e153c747c7895
zmark <351479188@qq.com>
2024-04-13 16:37:38 +0800

    feat: 完成NotFoundPage

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,13 +1,65 @@
 <script>
+import LoadingBoxPacking from "@/components/common/LoadingBoxPacking.vue";
+import ButtonHover1 from "@/components/common/ButtonHover1.vue";
+
 export default {
-	name: "NotFoundPage"
+	name: "NotFoundPage",
+	components: {
+		LoadingBoxPacking, ButtonHover1
+	},
+	data() {
+		return {
+			msg: "返 回 首 页"
+		};
+	},
+	methods: {
+		onClick() {
+			this.$router.push("/");
+		}
+	}
 };
 </script>
 
 <template>
-	<div>404 页面，可能没有</div>
+	<div id="not-found-page-container">
+		<div class="item" id="first">
+			<h1>页面不存在</h1>
+		</div>
+		<div class="item" id="first">
+			<ButtonHover1 :onClick="onClick" :msg="msg"/>
+		</div>
+		<div class="item">
+			<LoadingBoxPacking class="loading-box-packing"/>
+		</div>
+	</div>
 </template>
 
-<style scoped>
+<style>
+#not-found-page-container {
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
 
+#first {
+	margin-top: -100px;
+	margin-bottom: 100px;
+}
+
+h1 {
+	font-size: 100px;
+	color: rgba(0, 0, 0, 0.6);
+}
+
+a {
+	font-size: .7em;
+	font-weight: 500;
+	color: #2e2e2e;
+	text-decoration: none;
+	padding: 8px 15px;
+	border-radius: 20px;
+}
 </style>
