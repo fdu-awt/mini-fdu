2076f957ff8f97f358f543c906ab23a189d54308
zmark <351479188@qq.com>
2024-04-15 10:06:04 +0800

    feat: api返回假数据

--- src/api/user.js
+++ src/api/user.js
@@ -15,11 +15,26 @@ export function logIn(username, password){
 		username: username,
 		password: password,
 	};
-	return request({
-		url: '/user-service/log-in',
-		method: 'post',
-		data: data,
+	console.log(data);
+	return new Promise((resolve, reject) => {
+		resolve({
+			data: {
+				object: {
+					username: username,
+					token: password,
+					tokenExpireTime: '2021-12-31 23:59:59',
+				},
+				code: 200,
+				msg: '登陆成功',
+			}
+		});
+		reject('登陆失败');
 	});
+	// return request({
+	// 	url: '/user-service/log-in',
+	// 	method: 'post',
+	// 	data: data,
+	// });
 }
 
 // 注册
@@ -46,10 +61,24 @@ export function logOut() {
 
 // 获取用户详细信息
 export function getUserInfo() {
-	return request({
-		url: '/user-service/get-user-info',
-		method: 'get'
+	return new Promise((resolve, reject) => {
+		resolve({
+			data: {
+				object: {
+					username: 'admin',
+					email: '3@qq.com',
+					selfImage: '/万叶/万叶.pmx',
+				},
+				code: 200,
+				msg: '获取用户信息成功',
+			},
+		});
+		reject('获取用户信息失败');
 	});
+	// return request({
+	// 	url: '/user-service/get-user-info',
+	// 	method: 'get'
+	// });
 }
 
 // 修改个人信息
