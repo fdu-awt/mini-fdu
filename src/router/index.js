080298223b254617a0d2735754c9b0e9d6d0963b
zmark <351479188@qq.com>
2024-05-12 13:53:07 +0800

    refactor: 在router模块中处理API_EVENTS.UN_AUTH

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory  } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import HomePage from "@/pages/HomePage.vue";
 import AboutPage from "@/pages/AboutPage.vue";
 import LogInPage from "@/pages/LogInPage.vue";
@@ -6,23 +6,24 @@ import SignUpPage from "@/pages/SignUpPage.vue";
 import UserProfile from "@/pages/UserProfile.vue";
 import NotFoundPage from "@/pages/NotFoundPage.vue";
 import STORAGE from "../store";
-import { ElNotification } from 'element-plus';
+import {ElMessageBox, ElNotification} from 'element-plus';
+import apiEmitter, {API_EVENTS} from "@/event/ApiEventEmitter";
 
 // 定义路由
 const routes = [
-	{ path: '/', component: HomePage, meta: { requireAuth: true }},
-	{ path: '/home', component: HomePage, meta: { requireAuth: true }},
-	{ path: '/about', component: AboutPage, meta: { requireAuth: false }},
-	{ path: '/login', component: LogInPage, meta: { requireAuth: false }},
-	{ path: '/signup', component: SignUpPage, meta: { requireAuth: false }},
-	{ path: '/profile', component: UserProfile, meta: { requireAuth: true }},
+	{path: '/', component: HomePage, meta: {requireAuth: true}},
+	{path: '/home', component: HomePage, meta: {requireAuth: true}},
+	{path: '/about', component: AboutPage, meta: {requireAuth: false}},
+	{path: '/login', component: LogInPage, meta: {requireAuth: false}},
+	{path: '/signup', component: SignUpPage, meta: {requireAuth: false}},
+	{path: '/profile', component: UserProfile, meta: {requireAuth: true}},
 	// 捕获所有未处理的路径
-	{ path: '/:pathMatch(.*)*', component: NotFoundPage, meta: { requireAuth: false }},
+	{path: '/:pathMatch(.*)*', component: NotFoundPage, meta: {requireAuth: false}},
 ];
 
 // 创建路由实例并传递 `routes` 配置
 const router = createRouter({
-	history: createWebHistory (),
+	history: createWebHistory(),
 	routes: routes,
 });
 
@@ -40,12 +41,31 @@ router.beforeEach((to, from, next) => {
 			});
 			next({
 				path: 'login',
-				query: { redirect: to.fullPath }
+				query: {redirect: to.fullPath}
 			});
 		}
 	} else {
 		next();
 	}
+});
+
+// 监听未授权事件
+apiEmitter.on(API_EVENTS.UN_AUTH, (msg) => {
+	function notifyAndGoLogin() {
+		ElMessageBox.alert(msg, '未授权访问', {
+			confirmButtonText: '前往登录',
+			type: 'warning'
+		}).then(() => {
+			STORAGE.logOut();
+			router.push({
+				path: '/login',
+				query: {redirect: router.currentRoute.value.fullPath}
+			}).then();
+		}).catch(() => {
+			notifyAndGoLogin();
+		});
+	}
+	notifyAndGoLogin();
 });
 
 export default router;
