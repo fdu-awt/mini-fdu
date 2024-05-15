8969a0d66beac1f1840401d950fa12fc6ab95f62
zmark <351479188@qq.com>
2024-05-15 13:40:28 +0800

    feat: 新增GameEventEmitter处理游戏错误事件

---
+++ src/event/GameEventEmitter.js
@@ -0,0 +1,28 @@
+const GAME_EVENTS = Object.freeze({
+	NO_LOCAL_USER_ID: Symbol("no_local_user_id"),
+	// 其他错误
+	OTHER_ERROR: Symbol("other_error"),
+});
+
+class GameEventEmitter {
+	constructor() {
+		this.events = {};
+	}
+
+	on(event, listener) {
+		if (!this.events[event]) {
+			this.events[event] = [];
+		}
+		this.events[event].push(listener);
+	}
+
+	emit(event, ...args) {
+		const listeners = this.events[event];
+		if (listeners) {
+			listeners.forEach(listener => listener(...args));
+		}
+	}
+}
+
+export { GAME_EVENTS };
+export default new GameEventEmitter();
