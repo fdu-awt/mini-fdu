1adec4812355ca785d5040077edd0607de718b6e
zmark <351479188@qq.com>
2024-05-21 16:01:12 +0800

    chore: 全局导入element-plus

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';  // 确保引入了Element Plus的CSS
 import '@/styles/global.css'; // 引入全局样式
 
 createApp(App)
 	.use(router)
+	.use(ElementPlus)
 	.mount('#app');
