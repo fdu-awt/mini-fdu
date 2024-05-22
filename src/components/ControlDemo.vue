e3d8b529f01106fc0f78b8d6dd4add33a87f3981
zouyi73 <2168603151@qq.com>
2024-05-22 22:58:47 +0800

    complete info notify but not improve style

--- src/components/ControlDemo.vue
+++ src/components/ControlDemo.vue
@@ -7,6 +7,7 @@ import ChatBox from "@/components/ChatBox.vue";
 import gameEventEmitter, {GAME_EVENTS}  from "@/event/GameEventEmitter";
 import PostDialog from "@/components/PostDialog.vue";
 import ClubDialog from "@/components/ClubDialog.vue";
+import eventBus from '@/eventbus/eventBus.js';
 
 export default {
 	name: "ControlDemo",
@@ -25,9 +26,11 @@ export default {
 			socket: null,
 			canvasContainer: null,
 			showSettingDialog: false,
+			newMessageNotification: false, // 新消息通知的标记
 		};
 	},
 	mounted() {
+		eventBus.on('newMessage', this.handleNewMessage);
 		this.canvasContainer = document.getElementById('canvas-container');
 		this.game = new Game(this.canvasContainer, new GuangHuaLou() , new Lab1FbxSelfImageLoader());
 		this.listenKeyDown();
@@ -35,6 +38,10 @@ export default {
 		window.addEventListener('objectClicked', (event) => {
 			this.handleObjectClick(event.detail.localId, event.detail.remoteId, event.detail.socket);
 		});
+	},
+	unmounted() {
+		// 组件卸载前，移除事件监听
+		eventBus.off('newMessage', this.handleNewMessage);
 	},
 	methods: {
 		listenKeyDown(){
@@ -59,7 +66,24 @@ export default {
 		},
 		closeChatBox() {
 			this.isChatBoxVisible = false; // 关闭ChatBox的方法
-		}
+		},
+		handleNewMessage(message, localId, remoteId, socket) {
+			// 如果ChatBox未显示，则设置新消息通知标志为true
+			if (!this.isChatBoxVisible) {
+				this.newMessageNotification = true;
+				this.localId = localId; // 可选：如果需要在通知中使用localId
+				this.remoteId = remoteId; // 可选：如果需要在通知中使用remoteId
+				this.socket = socket; // 可选：如果需要在通知中使用socket
+			}
+			// 这里可以添加其他处理新消息的逻辑
+		},
+		// ...其他已有方法
+		viewNewMessage() {
+			// 用户点击查看新消息通知时调用的方法
+			this.isChatBoxVisible = true;
+			this.newMessageNotification = false;
+			// 这里可以添加打开ChatBox后需要执行的逻辑
+		},
 	},
 };
 </script>
@@ -72,11 +96,34 @@ export default {
 	
 	<SettingDialog :show="showSettingDialog" @close="onSettingDialogClose"/>
 	<ChatBox v-if="isChatBoxVisible" :socket="socket" :remote-id="remoteId" :local-id="localId" @close="closeChatBox" />
+  <div v-if="newMessageNotification" class="new-message-notification">
+    您有一条新消息，请点击查看。
+    <button @click="viewNewMessage">查看消息</button>
+  </div>
 </template>
 
 <style scoped>
 #canvas-container {
   width: 100%;
   height: 100%;
+}
+.new-message-notification {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: #f44336; /* 红色背景 */
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+
+.new-message-notification button {
+  margin-left: 10px;
+  background-color: #ffeb3b; /* 按钮颜色 */
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
 }
 </style>
