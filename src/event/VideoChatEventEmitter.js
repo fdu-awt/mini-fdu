a157fda92ca110fce3016553f7bcb0ba5152b0f6
zmark <351479188@qq.com>
2024-05-31 16:49:36 +0800

    feat: 新增VideoChatEventEmitter

---
+++ src/event/VideoChatEventEmitter.js
@@ -0,0 +1,16 @@
+import EventEmitter from "@/event/EventEmitter";
+
+const VIDEO_CHAT_EVENTS = Object.freeze({
+	START: Symbol("start_video_chat"),
+	REJECTED: Symbol("video_rejected"),
+	END: Symbol("end_video_chat"),
+});
+
+class VideoChatEventEmitter extends EventEmitter {
+	constructor() {
+		super();
+	}
+}
+
+export { VIDEO_CHAT_EVENTS };
+export default new VideoChatEventEmitter();
