f68e1583968b15b9709427611592a6589a8664d8
zmark <351479188@qq.com>
2024-05-31 19:41:09 +0800

    feat: video-chat-demo

--- src/components/VideoChatDemo.vue
+++ src/components/VideoChatDemo.vue
@@ -1,6 +1,9 @@
 <script>
 import videoChatService from "@/api/socket/VideoChatService";
 import store from "@/store";
+import videoChatEventEmitter, {VIDEO_CHAT_EVENTS} from "@/event/VideoChatEventEmitter";
+import {ElMessageBox} from "element-plus";
+import gameEventEmitter from "@/event/GameEventEmitter";
 
 export default {
 	name: "VideoChatDemo",
@@ -15,14 +18,64 @@ export default {
 			const userId = store.getUserId();
 			let toId = userId + 1;
 			store.setUserId(userId + 1);
-			const localVideo = document.getElementById('localVideo');
-			const remoteVideo = document.getElementById('remoteVideo');
+			videoChatService.userId = userId;
 			startButton.addEventListener('click', () => {
-				videoChatService.invite(toId, localVideo, remoteVideo);
+				videoChatEventEmitter.emit(VIDEO_CHAT_EVENTS.START, toId);
 			});
 			hangupButton.addEventListener('click', () => {
 				videoChatService.hangup();
 			});
+			videoChatEventEmitter.on(VIDEO_CHAT_EVENTS.START, this.onChatStart.bind(this));
+			videoChatEventEmitter.on(VIDEO_CHAT_EVENTS.INVITE, this.onChatInvite.bind(this));
+			videoChatEventEmitter.on(VIDEO_CHAT_EVENTS.END, () => {
+				ElMessageBox.alert('对方已挂断', '视频聊天结束', {
+					confirmButtonText: '确定',
+					type: 'warning',
+				}).then(() => {
+					this.visible = false;
+				}).catch(() => {
+					this.visible = false;
+				});
+			});
+			videoChatEventEmitter.on(VIDEO_CHAT_EVENTS.REJECTED, (msg) => {
+				ElMessageBox.alert(msg, '视频聊天被拒绝', {
+					confirmButtonText: '确定',
+					type: 'warning',
+				}).then(() => {
+					this.visible = false;
+				}).catch(() => {
+					this.visible = false;
+				});
+			});
+		},
+		onChatStart(toId) {
+			gameEventEmitter.requestAllControl();
+			this.visible = true;
+			this.$nextTick(() => {
+				const localVideo = document.getElementById('localVideo');
+				console.log("localVideo", localVideo);
+				const remoteVideo = document.getElementById('remoteVideo');
+				videoChatService.invite(toId, localVideo, remoteVideo);
+			});
+		},
+		onChatInvite(fromId){
+			const onAccept = () => {
+				this.visible = true;
+				this.$nextTick(() => {
+					const localVideo = document.getElementById('localVideo');
+					const remoteVideo = document.getElementById('remoteVideo');
+					videoChatService.accept(fromId, localVideo, remoteVideo);
+				});
+			};
+			const onReject = () => {
+				videoChatService.reject(fromId);
+			};
+			// TODO 加入用户名的显示
+			ElMessageBox.confirm('收到视频聊天邀请，是否接受？', '视频聊天邀请', {
+				confirmButtonText: '接受',
+				cancelButtonText: '拒绝',
+				type: 'warning',
+			}).then(onAccept).catch(onReject);
 		},
 	},
 };
